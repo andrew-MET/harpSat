d3192dff60f2f9856b39617e1444ca61e9e3b01d
Andrew Singleton <andrewts@met.no>
2021-11-15 10:45:30 +0100

    Add first functions

---
+++ R/read_ascat.R
@@ -0,0 +1,168 @@
+#' Title
+#'
+#' @param file_name
+#' @param parameter
+#' @param rounding
+#' @param bounding_date_times
+#' @param bounding_domain
+#' @param resample_domain
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_ascat_nc <- function(
+  file_name,
+  parameter,
+  rounding            = c(NA, "secs", "mins", "hours"),
+  bounding_date_times = NA,
+  bounding_domain     = NA,
+  resample_domain     = NA,
+  lonlat_to_id        = FALSE
+) {
+
+  rounding <- match.arg(rounding)
+
+  has_bbox <- TRUE
+
+  if (!is.na(bounding_date_times)) {
+    if (length(bounding_date_times) != 2) {
+      stop("`bounding_date_times` must be a vector of length 2")
+    }
+    bounding_date_times <- sort(harpIO::str_datetime_to_datetime(bounding_date_times))
+  }
+
+  bounding_domain_try <- try(meteogrid::as.geodomain(bounding_domain), silent = TRUE)
+  if (inherits(bounding_domain_try, "try-error")) {
+    if (!is.na(bounding_domain)) {
+      stop("`bounding_domain` must be a `geofield` or `geodomain`")
+    }
+    has_bbox <- FALSE
+    bounding_domain_try <- NA
+  }
+  bounding_domain <- bounding_domain_try
+
+  resample_domain_try <- try(meteogrid::as.geodomain(resample_domain), silent = TRUE)
+  if (inherits(resample_domain_try, "try-error")) {
+    if (!is.na(resample_domain)) {
+      stop("`resample_domain` must be a `geofield` or `geodomain`")
+    }
+    resample_domain <- NA
+  }
+  resample <- FALSE
+  if (inherits(resample_domain_try, "geodomain")) {
+    resample_domain <- resample_domain_try
+    bounding_domain <- resample_domain
+    resample        <- TRUE
+    has_bbox        <- TRUE
+  }
+
+  nc_id <- ncdf4::nc_open(file_name)
+
+  nc_vars <- names(nc_id[["var"]])
+  params  <- intersect(parameter, nc_vars)
+
+  if (length(params) < 1) {
+    warning("None of the requested paramters found in file.", immediate. = TRUE)
+    ncdf4::nc_close(nc_id)
+    return(NULL)
+  }
+
+  missing_params <- setdiff(sort(parameter), sort(params))
+
+  if (length(missing_params) > 0) {
+    warning("`", paste(missing_params, sep = "`, `"), "` not found in file.", immediate. = TRUE)
+  }
+
+  times      <- ncdf4::ncvar_get(nc_id, "time")
+  time_units <- ncdf4::ncatt_get(nc_id, "time", "units")
+
+  origin <- sub("^ ", "", gsub("[[:alpha:]]+ since", "", time_units[["value"]]))
+  times  <- times * harpIO:::units_multiplier(substr(time_units[["value"]], 1, 1))
+  times  <- as.POSIXct(times, tz = "UTC", origin = origin)
+
+  if (!is.na(rounding)) {
+    times <- round(times, rounding)
+  }
+
+  time_range <- range(times)
+
+  if (!is.na(bounding_date_times)) {
+    if (max(time_range) < min(bounding_date_times) | min(time_range) > max(bounding_date_times)) {
+      warning(
+        "Times in file are outside of `bounding_date_times`.\n",
+        "`bounding_date_times`: ", paste(bounding_date_times, collapse = " - "), "\n",
+        "Time range in file   : ", paste(time_range, collapse = " - "),
+        immediate. = TRUE
+      )
+      ncdf4::nc_close(nc_id)
+      return(NULL)
+    }
+  }
+
+  lat <- ncdf4::ncvar_get(nc_id, "lat")
+  lon <- ncdf4::ncvar_get(nc_id, "lon")
+
+  if (has_bbox) {
+
+    indices <- meteogrid::point.index(
+      lon    = as.vector(lon),
+      lat    = as.vector(lat),
+      domain = bounding_domain
+    )
+
+    if (!any(complete.cases(indices))) {
+      warning("Whole swath is outside of `bounding_domain`.", immediate. = TRUE)
+      ncdf4::nc_close(nc_id)
+      return(NULL)
+    }
+
+  }
+
+  swath_df <- tibble::tibble(
+    validdate = times[1:length(times)],
+    lon       = lon[1:length(lon)],
+    lat       = lat[1:length(lat)],
+    SID       = seq_along(times)
+  )
+
+  if (lonlat_to_id) {
+    swath_df[["SID"]] <- paste(
+      round(swath_df[["lon"]], 5), round(swath_df[["lat"]], 5), sep = "_"
+    )
+  }
+
+  for (param in params) {
+    swath_df[[param]] <- as.vector(ncdf4::ncvar_get(nc_id, param))
+  }
+
+  ncdf4::nc_close(nc_id)
+
+  if (has_bbox) {
+    swath_df <- cbind(swath_df, indices)
+    swath_df <- dplyr::filter(
+      swath_df,
+      dplyr::if_all(dplyr::all_of(c("i", "j")), ~!is.na(.x))
+    )
+    swath_df <- cbind(
+      swath_df,
+      meteogrid::project(swath_df$lon, swath_df$lat, bounding_domain[["projection"]])
+    )
+    attr(swath_df, "domain") <- bounding_domain
+  }
+
+  if (!resample) {
+    return(
+      tibble::as_tibble(
+        dplyr::filter(
+          swath_df, dplyr::if_any(dplyr::all_of(params), ~!is.na(.x))
+        )
+      )
+    )
+  }
+
+  tibble::as_tibble(
+    dplyr::filter(swath_df, dplyr::if_any(dplyr::all_of(params), ~!is.na(.x)))
+  )
+
+}
---
+++ R/swath_to_geofield.R
@@ -0,0 +1,60 @@
+swath_to_geofield <- function(swath_df, data_col, dom, group_col, ...) {
+
+  data_col_quo  <- rlang::enquo(data_col)
+  group_col_quo <- rlang::enquo(group_col)
+
+  data_col_name  <- rlang::as_label(data_col_quo)
+  expected_cols <- c("lon", "lat", data_col_name)
+
+  if (
+    !rlang::quo_is_null(group_col_quo) &&
+      !rlang::quo_is_missing(group_col_quo)
+  ) {
+    group_col_name <- rlang::as_label(group_col_quo)
+    expected_cols <- c(expected_cols, group_col_name)
+  }
+
+  missing_cols  <- setdiff(expected_cols, colnames(swath_df))
+
+  if (length(missing_cols) > 0) {
+    stop("`swath_df` must contain columns: `", paste(expected_cols, collapse = "`, `"), "`")
+  }
+
+  dom <- try(meteogrid::as.geodomain(dom))
+  if (inherits(dom, "try-error")) {
+    stop("`dom` must be a `geofield` or `geodomain`.")
+  }
+
+  indices <- meteogrid::point.index(
+    swath_df$lon,
+    swath_df$lat,
+    domain = dom
+  )
+
+  if (length(which(!is.na(indices[["i"]]) && !is.na(indices[["j"]]))) < 1) {
+    stop("Whole swath is outside of domain.", immediate. = TRUE)
+  }
+
+  swath_raster <- harpIO::geofield_to_raster(
+    meteogrid::as.geofield(NA_real_, dom)
+  )
+
+  proj4str <- meteogrid::proj4.list2str(dom$projection)
+
+  coords   <- swath_df[c("lon", "lat")]
+  swath_df <- swath_df[!colnames(swath_df) %in% c("lon", "lat")]
+
+  swath_df <- sp::SpatialPointsDataFrame(
+    coords      = data.frame(coords),
+    data        = data.frame(swath_df),
+    proj4string = sp::CRS("+proj=longlat")
+  ) %>%
+    sp::spTransform(proj4str)
+
+  swath_raster <- raster::rasterize(
+    swath_df, swath_raster, field = data_col_name#, fun = mean
+  )
+
+  harpIO::raster_to_geofield(swath_raster)
+
+}
